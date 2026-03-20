d6d15bf84046a5e0c70cb72239db54685ff246d4
daniel-silva-rodrigues <danielsilvarodrigues26@gmail.com>
2026-03-20 10:56:34 -0300

    adding the questions and options

--- trivia.js
+++ trivia.js
@@ -1,3 +1,50 @@
+//declaração de variáveis
+let q = -1;
+let answer;
+let correct;
+let correctQuestions;
+let qNumber = document.getElementById("questionNumber");
+let qText = document.getElementById("questionText");
+let options = {
+    optionA: document.getElementById("option1"),
+    optionB: document.getElementById("option2"),
+    optionC: document.getElementById("option3")
+}
+let questions = {
+    number: [1, 2 , 3],
+    text: ["Which one's is the most famous dinosaur?", "Which of these dinosaurs was a herbivore?", "Which of this dinosaurs had a armor?"],
+    first: ["Tiranosaur", "Velociraptor","Spinosaur"],
+    second: ["Megalosaur", "Baryonix", "Ankilosaur"],
+    third: ["Estegosaur", "Triceratops", "Brachiosaur"],
+    expected: [0, 2, 1]
+}
+//função que alterna entre a página inicial e a de perguntas
 function mudarPagina() {
     window.location.href = "perguntas.html";
+    update();
+}
+//funções que marcam as alternativas
+function markA() {
+    answer = 0;
+}
+function markB() {
+    answer = 1;
+}
+function markC() {
+    answer = 2;
+}
+//função que submete as respostas
+function update () {
+    q++;
+    qNumber.innerHTML = questions.number[q];
+    qText.innerHTML = questions.text[q];
+    optionA.innerHTML = questions.first[q];
+    optionB.innerHTML = questions.second[q];
+    optionC.innerHTML = questions.third[q];
+    if (answer === questions.expected[q]) {
+        correct = true;
+        correctQuestions++;
+    } else {
+        correct = false
+    }
 }
